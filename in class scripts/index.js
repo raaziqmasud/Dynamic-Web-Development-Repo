141c90eff1bba9a50e9a3f714bd450b982b739bc
Raaziq Brown <raaziqbrown@10-17-58-183.dynapool.wireless.nyu.edu>
2019-04-08 17:51:32 -0400

    latest commit

--- in class scripts/index.js
+++ in class scripts/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var Mus= require('mustache-express');
 
 var app = express()
+
+app.engine('html', mustacheExpress());
+app.set('view.engine', 'html');
+app.set('views',__dirname);
 
     //
 app.get('/', function(req, res){
 
     //can fill with html content
 res.send("hey");})
+res.render('form')
 
 app.listen(8000, function(){
 console.log('serving websites and not drugs')
