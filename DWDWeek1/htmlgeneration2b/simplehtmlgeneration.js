2b4893f4e1515d12bf3cd213d1c6fcac9ac24fbc
Raaziq Brown <raaziqbrown@Raaziqs-Air-396.home>
2019-04-08 23:11:57 -0400

    another commit

--- DWDWeek1/htmlgeneration2b/simplehtmlgeneration.js
+++ DWDWeek1/htmlgeneration2b/simplehtmlgeneration.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var html = "<html><head></head><body><h1>HOLA WORLD</h1></body></html>";
+var html = "<html><head></head><body><h1>I dont know if it works but it will. I dont know its broken but it works. hello world it makes sense.</h1></body></html>";
 
 fs.writeFile('hey.html', html, function(err) {
     if (err)
