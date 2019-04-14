80d3cdff6cde2b2ec9e29d775d34a97ff8d77727
Raaziq Brown <raaziqbrown@Raaziqs-Air-396.home>
2019-04-14 15:02:53 -0400

    another

---
+++ DWDWeek2/PArtB.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var app = express()
+var insults = ["garbaggio","cancelled","horrendous"];
+
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+app.get('/', function(req, res) {
+    res.render('PartB', {
+      bandName: req.query.form_input_band
+    });
+  });
+
+app.get('/answer', function(req, res){
+    var bandName = req.query.band
+    console.log(req.query.band)
+    var x = Math.floor(Math.random() * 3);
+    res.send("I heard " + bandName + " is " + insults[x]);
+})
+
+
+app.listen(8000, function () {
+  console.log('Our first web server! Started on port 8000')})
