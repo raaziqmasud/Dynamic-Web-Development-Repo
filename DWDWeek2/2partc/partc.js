3f9669258667ccfc3f16d07ea5701d942ff5a29b
Raaziq Brown <raaziqbrown@10-17-66-112.dynapool.wireless.nyu.edu>
2019-04-15 17:36:22 -0400

    yay

--- DWDWeek2/2partc/partc.js
+++ DWDWeek2/2partc/partc.js
@@ -2,9 +2,22 @@ var express = require('express');
 var mustacheExpress = require('mustache-express');
 var app = express()
 
+
+
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
+
+app.get('partc', function (req, res) {
+
+if (req.query.url){
+request(req.query.url, function(error, response, html){
+res.send(html);
+
+});
+} else{res.render('partc');}
+
+});
 
 app.get('/', function(req, res) {
   res.render('partc', {
